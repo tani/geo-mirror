c8e0ff8b1b391261eeca1e5b1031ada55c361bd4
TANIGUHCI Masaya <ta2gch@gmail.com>
2023-06-14 22:25:10 +0900

    Initial commit

--- src/worker.js
+++ src/worker.js
@@ -1,15 +1,54 @@
 /**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
+ * getArchMirror
+ * @param {string} country
+ * @returns Promise<string>
  */
+export async function getArchMirror(country) {
+    const src = await fetch('https://archlinux.org/mirrorlist/?ip_version=4').then(r => r.text())
+    const re = /Server = (https?:\/\/.*)\/\$repo\/os\/\$arch/g
+    const mirrors = {}
+    let match
+    while (match = re.exec(src)) {
+        const url = new URL(match[1])
+        const tld = url.hostname.split('.').pop()
+        mirrors[tld] = url.toString()
+    }
+    if (country.toLowerCase() in mirrors) {
+      return mirrors[country.toLowerCase()]
+    } else {
+      return 'https://geo.mirror.pkgbuild.com'
+    }
+}
+
+
+/**
+ * getDebianMirror
+ * @param {string} country
+ * @returns Promise<string>
+ */
+export function getDebianMirror(country) {
+    return Promise.resolve(`https://ftp.${country.toLowerCase()}.debian.org`)
+}
+
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+  /**
+   * fetch
+   * @param {Request} request
+   * @returns {Promise<Response>}
+   */
+  async fetch(request) {
+    const url = new URL(request.url);
+    const country = /** @type {string} */ (request.cf.country);
+    if (url.pathname.startsWith('/archlinux')) {
+      const pathname =url.pathname.split('/').slice(2).join('/')
+      const redirect = `${await getArchMirror(country)}/${pathname}`
+      return Response.redirect(redirect);
+    }
+    if (url.pathname.startsWith('/debian')) {
+      const pathname =url.pathname.split('/').slice(2).join('/')
+      const redirect = `${await getDebianMirror(country)}/${pathname}`
+      return Response.redirect(await getDebianMirror(country));
+    }
+  }
 };
